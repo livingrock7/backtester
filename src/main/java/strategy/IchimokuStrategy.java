81d9cff49e4e0ec8857322b65026893cf0a3a241
livingrock7 <chiragtitiya@exvillage.com>
2020-06-01 13:46:50 +0530

    ichimoku correction

--- src/main/java/strategy/IchimokuStrategy.java
+++ src/main/java/strategy/IchimokuStrategy.java
@@ -20,13 +20,19 @@ public class IchimokuStrategy implements StrategyRule{
     public BaseStrategy getLongStrategy(BarSeries series) {
         // tested on daily 6 aug 2019 to 15 may 2020 // needs to tune and combine indicators
         ClosePriceIndicator close = new ClosePriceIndicator(series);
-        IchimokuKijunSenIndicator maroonLine = new IchimokuKijunSenIndicator(series);
-        IchimokuTenkanSenIndicator blueLine = new IchimokuTenkanSenIndicator(series);
-        IchimokuSenkouSpanAIndicator greenLine = new IchimokuSenkouSpanAIndicator(series);
+        IchimokuKijunSenIndicator maroonLine = new IchimokuKijunSenIndicator(series,26);
+        IchimokuTenkanSenIndicator blueLine = new IchimokuTenkanSenIndicator(series,9);
+        IchimokuSenkouSpanAIndicator greenLine = new IchimokuSenkouSpanAIndicator(series,blueLine,maroonLine);
         IchimokuSenkouSpanBIndicator redLine = new IchimokuSenkouSpanBIndicator(series);
         DifferenceIndicator diff = new DifferenceIndicator(blueLine,redLine);
         AveragePriceIndicator averagePriceIndicator = new AveragePriceIndicator(series);
-        Rule entryRule = new OverIndicatorRule(close,greenLine).and(new OverIndicatorRule(close,redLine)).and(new CrossedUpIndicatorRule(close,blueLine).and(new OverIndicatorRule(blueLine,maroonLine)));
+        System.out.println(maroonLine.getValue(100));
+        System.out.println(greenLine.getValue(100));
+        System.out.println(blueLine.getValue(100));
+        System.out.println(redLine.getValue(100));
+        //Rule entryRule = new OverIndicatorRule(close,greenLine).and(new OverIndicatorRule(close,redLine)).and(new CrossedUpIndicatorRule(close,blueLine).and(new OverIndicatorRule(blueLine,maroonLine)));
+        //Rule entryRule = new OverIndicatorRule(close,greenLine).and(new OverIndicatorRule(close,redLine)).and(new CrossedUpIndicatorRule(close,blueLine).and(new OverIndicatorRule(blueLine,maroonLine)));
+        Rule entryRule = new OverIndicatorRule(blueLine,greenLine).and(new OverIndicatorRule(blueLine,redLine)).and(new OverIndicatorRule(maroonLine,greenLine)).and(new OverIndicatorRule(maroonLine,redLine)).and(new OverIndicatorRule(close,blueLine));
         Rule exitRule = new UnderIndicatorRule(close,blueLine)
                 .or(new StopGainRule(close, PrecisionNum.valueOf(35)))
                 .or(new StopLossRule(close, PrecisionNum.valueOf(1.5)));
@@ -43,6 +49,7 @@ public class IchimokuStrategy implements StrategyRule{
         DifferenceIndicator diff = new DifferenceIndicator(blueLine,redLine);
         AveragePriceIndicator averagePriceIndicator = new AveragePriceIndicator(series);
         Rule entryRule = new UnderIndicatorRule(close,greenLine).and(new UnderIndicatorRule(close,redLine)).and(new CrossedDownIndicatorRule(close,maroonLine)).and(new UnderIndicatorRule(blueLine,maroonLine));
+        //Rule entryRule = new UnderIndicatorRule(close,greenLine).and(new UnderIndicatorRule(close,redLine)).and(new CrossedDownIndicatorRule(close,blueLine)).and(new UnderIndicatorRule(close,blueLine)).and(new UnderIndicatorRule(close,maroonLine));
         Rule exitRule = new UnderIndicatorRule(blueLine,close)
                 .or(new StopGainRule(close, PrecisionNum.valueOf(5)))
                 .or(new StopLossRule(close, PrecisionNum.valueOf(1.5)));
